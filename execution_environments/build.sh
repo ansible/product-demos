6c65b53ac98327a495d3e8ecd57f6cb97a9b4e3d
Chris Edillon <67980205+jce-redhat@users.noreply.github.com>
2024-09-23 15:15:53 -0400

    added local build script for product demos EEs (#184)

---
+++ execution_environments/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# array of images to build
+ee_images=(
+    "apd-ee-24"
+)
+
+for ee in "${ee_images[@]}"
+do
+    # build EE image
+    ansible-builder build \
+        --file ${ee}.yml \
+        --context ./ee_contexts/${ee} \
+        --build-arg ANSIBLE_GALAXY_SERVER_CERTIFIED_TOKEN \
+        --build-arg ANSIBLE_GALAXY_SERVER_VALIDATED_TOKEN \
+        -v 3 \
+        -t quay.io/ansible-product-demos/${ee}:$(date +%Y%m%d)
+
+    if [[ $? == 0 ]]
+    then
+        # tag EE image as latest
+        podman tag \
+            quay.io/ansible-product-demos/${ee}:$(date +%Y%m%d) \
+            quay.io/ansible-product-demos/${ee}:latest
+    fi
+done
