c7268bad64c8303fafa6a8e5738f8430e19d25f4
matt <l3acon@gmail.com>
2025-05-02 10:32:55 -0600

    try to fix 24

--- .github/workflows/run-pc.sh
+++ .github/workflows/run-pc.sh
@@ -5,6 +5,9 @@ PYTHON_VARIANT="${USE_PYTHON:-python3.11}"
 # intsall pip
 eval "${PYTHON_VARIANT} -m pip install --upgrade pip"
 
+# try to fix 2.4 incompatibility 
+eval "${PYTHON_VARIANT} -m pip install --upgrade setuptools wheel twine check-wheel-contents"
+
 # intsall pre-commit
 eval "${PYTHON_VARIANT} -m pip install pre-commit"
 
