3a9e0f9b04f0eeccf81dca553eac9555965ec664
matt <l3acon@gmail.com>
2026-01-08 09:55:57 -0700

    this is simpler, try adding more

--- .github/workflows/run-pc.sh
+++ .github/workflows/run-pc.sh
@@ -9,7 +9,7 @@ PATH="$PATH:$HOME/.local/bin"
 # intsall pip
 eval "${PYTHON_VARIANT} -m pip install --user --upgrade pip"
 
-# try to fix 2.4 incompatibility 
+# try to fix 2.4 incompatibility
 eval "${PYTHON_VARIANT} -m pip install --user --upgrade setuptools wheel twine check-wheel-contents"
 
 # intsall pre-commit
@@ -22,4 +22,4 @@ eval "${PYTHON_VARIANT} -m pip freeze --local"
 git config --global --add safe.directory $(pwd)
 
 # run pre-commit
-pre-commit run --config $(pwd)/.pre-commit-gh.yml --show-diff-on-failure --color=always 
+pre-commit run --config $(pwd)/.pre-commit-gh.yml --show-diff-on-failure --color=always
