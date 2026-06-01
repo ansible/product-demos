f599f1651cdfa4450b7c06af8e7bfc56b1226dbb
Chris Edillon <67980205+jce-redhat@users.noreply.github.com>
2026-06-01 16:36:48 -0400

    Updated EE definition (#300)

--- .github/workflows/run-pc.sh
+++ .github/workflows/run-pc.sh
@@ -3,13 +3,13 @@
 # should no longer need this
 #dnf install git-lfs -y
 
-PYTHON_VARIANT="${USE_PYTHON:-python3.11}"
+PYTHON_VARIANT="${USE_PYTHON:-python3.12}"
 PATH="$PATH:$HOME/.local/bin"
 
 # intsall pip
 eval "${PYTHON_VARIANT} -m pip install --user --upgrade pip"
 
-# try to fix 2.4 incompatibility 
+# try to fix 2.4 incompatibility
 eval "${PYTHON_VARIANT} -m pip install --user --upgrade setuptools wheel twine check-wheel-contents"
 
 # intsall pre-commit
@@ -22,4 +22,4 @@ eval "${PYTHON_VARIANT} -m pip freeze --local"
 git config --global --add safe.directory $(pwd)
 
 # run pre-commit
-pre-commit run --config $(pwd)/.pre-commit-gh.yml --show-diff-on-failure --color=always 
+pre-commit run --config $(pwd)/.pre-commit-gh.yml --show-diff-on-failure --color=always
